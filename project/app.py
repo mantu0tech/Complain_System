4b5de52d21c982d86cd13e13dd225e52414ae2c7
mantu0tech <mantashaansari246@gmail.com>
2026-06-27 09:43:36 +0530

    data

--- project/app.py
+++ project/app.py
@@ -12,6 +12,7 @@ load_dotenv()
 
 # --- Configuration ---
 
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -44,6 +45,8 @@ for var in required_vars:
         raise ValueError(f"Missing environment variable: {var}")
 # --- Initialize extensions ---
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
